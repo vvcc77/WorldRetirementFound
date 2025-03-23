9dcc99f41794c555cbbc6776ff3fee5f303d78e6
vvcc77 <112783656+vvcc77@users.noreply.github.com>
2025-03-23 05:01:50 -0300

    Add files via upload

---
+++ contract/WorldRetirementFund.sol
@@ -0,0 +1,19 @@
+Vamos a responder la siguiente pregunta:
+Tell us about you and your co-founder(s).
+*
+How you met, how long have you worked together and how long have you worked on this project? 
+
+Respuesta: Soy Viviana Cortez, Jujeña, emprendedora y abuela. En 2017 conocí la cripto moneda y me apasione con la block chain, conocí a mi co-founder en 2018 en un curso de cripto monedas ambos compartíamos el interés mas por la tecnología block chain que por la cripto moneda, comenzamos una relación donde compartíamos conocimientos y seguidamente comenzaron los proyectos. Durante 2022 en pleno VIR Market decidimos que era el momento correcto para crear un producto para que cuando llegue el BULL  tener nuestro producto listo, comenzamos a analizar la problemáticas buscando dar usabilidad a la tagnologia block chain luego de una ardua investigación de mercado decidimos en 2023 crear la primera versión de mvp de Appertraz.
+
+
+En logro 2021, diseñé un programa de estudio sobre Usabilidad de Bitcoin y Ley Bitcoin para la Fundación Empresarial para el Desarrollo Educativo de El Salvador. También desarrollé un programa de Criptofinanzas para el Instituto de Capacitación y Desarrollo Empresarial en Posadas, Misiones.
+
+Durante 2024 enfrenté situaciones extremadamente estresntes, a principio de año estuve internada 1 mes y medio y sometida a 3 operaciones quirurgicas, durante el mes de julio murio mi padre repentinamente de una paro cardiaco, todo eso me levó a un profundo análisis de mis propósitos, me ayudó mucho la meditaci{on, el psicoan{alisi y el ejercicio. Con un propósito claro seguí avanzando en la segunda mitad del año donde profundice mis conocimeintos en startup y emprendedurismo y termine siendo  seleccionada entre 600 postulantes para el programa WE de Endeavor Argentina, destacándome como una de las 45 emprendedoras elegidas. Al finalizar el programa, mi proyecto Appertraz fue uno de los cinco seleccionados para el Demo Day en las oficinas de Globant en Buenos Aires, un hito clave en el desarrollo del proyecto.
+
+Dario Byczko is a food engineer with an outstanding academic record, achieving the highest GPA in his master's program with a grade of 97.3%. His thesis focused on the "Development of a Food Safety System Applied to Small and Medium Yerba Mate Establishments" in Posadas, Misiones.
+
+Additionally, he was awarded 1st Prize for two consecutive years (2010 and 2011) in the Student Contest for Innovative Food Products, excelling with his developments of buffalo milk powder and tree tomato powder.
+
+Dario was also recognized by the Chamber of Deputies of the Province of Misiones for his academic achievements and the awards he received, further highlighting his significant contributions to the field.
+
+Para el manejo de las situaciones estresantes Dario se apoyó en el canotaje y el Trail runnig, realizando travesías de canotaje como recorrer el rio parana desde iguazu hasta posadas y participo de varias competencia de campo traviesa logrando llegar entre klos primeros lugares.
